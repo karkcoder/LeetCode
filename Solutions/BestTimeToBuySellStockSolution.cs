47b3f6a1d2d4447da21839785caa824e34973846
karkcoder <karkcoder@users.noreply.github.com>
2023-07-24 15:22:08 -0600

    O(n) solution (#10)

---
+++ Solutions/BestTimeToBuySellStockSolution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Solutions
+{
+    //You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+    //You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+    //Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.
+
+    //Example 1:
+
+    //Input: prices = [7, 1, 5, 3, 6, 4]
+    //Output: 5
+    //Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    //Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+    //Example 2:
+
+    //Input: prices = [7,6,4,3,1]
+    //    Output: 0
+    //Explanation: In this case, no transactions are done and the max profit = 0.
+
+    public partial class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int lPointer = 0; //Initializing to first day
+            int rPointer = 1; //Initializing to second day
+
+            int bestProfit = 0;
+
+            while (rPointer < prices.Length)
+            {
+                if (prices[lPointer] < prices[rPointer])
+                {
+                    var profit = prices[rPointer] - prices[lPointer];
+                    bestProfit = Math.Max(bestProfit, profit);
+                }
+                else
+                {
+                    lPointer = rPointer;
+                }
+                rPointer++;
+            }
+            return bestProfit;
+        }
+    }
+}
